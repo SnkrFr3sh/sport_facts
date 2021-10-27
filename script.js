2236070aac919d31bac6046ad0fd73f05050d6e1
Newton Hoang <newtonhoang99@gmail.com>
2021-10-26 19:32:27 -0700

    google fill now uses data from NBA api rather than search value and adds nba to end of search

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ var picture1 = document.getElementById("picture1");
 var picture2 = document.getElementById("picture2");
 var picture3 = document.getElementById("picture3");
 // var picture4 = document.getElementById("picture4");
-var fetchButton = document.getElementById("search-btn");
+var googleSearch = "";
 
 // NBA API Variables
 var nbaUrl = "https://free-nba.p.rapidapi.com/players?page=0&per_page=25";
 var playerSearch = "";
-var searchValue = $("#search-input")
-var searchBtn = $("#search-btn")
-var plyrName = $("#player-name")
-var plyrTeam = $("#team")
-var plyrPosi = $("#position")
-var plyrConf= $("#conference")
-var plyrHeight = $("#height")
+var searchValue = $("#search-input");
+var searchBtn = $("#search-btn");
+var plyrName = $("#player-name");
+var plyrTeam = $("#team");
+var plyrPosi = $("#position");
+var plyrConf= $("#conference");
+var plyrHeight = $("#height");
 
+//Player Information Fill
 function searchPlayer(){
     playerSearch = searchValue.val();
     playerSearch = playerSearch.toLowerCase();
     playerSearch = encodeURIComponent(playerSearch.trim());
 
-    //Player Information Fill
 
     fetch(nbaUrl + "&search=" + playerSearch, {
         "method": "GET",
@@ -35,7 +35,7 @@ function searchPlayer(){
       })
     .then(function (data) {
         console.log(data.data);
-        
+
         plyrName.text(data.data[0].first_name + " " + data.data[0].last_name);
         plyrTeam.text("Current Team: " + data.data[0].team.full_name);
         if(data.data[0].position == ""){
@@ -44,7 +44,7 @@ function searchPlayer(){
         plyrPosi.text("Position: " + data.data[0].position);
         plyrConf.text("Conference: " + data.data[0].team.conference);
         if(data.data[0].height_feet !== null){
-            plyrPosi.text("Height: N/A");
+            plyrHeight.text("Height: N/A");
         }else
         plyrHeight.text("Height: " + data.data[0].height_feet + "" + data.data[0].height_inches);
 
@@ -55,33 +55,38 @@ function searchPlayer(){
         //     plyrConf.text("Conference: " + data.data[i].team.conference);
         //     plyrHeight.text("Height: " + data.data[i].height_feet + "" + data.data[i].height_inches);
         // }
+        googleSearch = data.data[0].first_name + "%20" + data.data[0].last_name + "%20nba";
+        googleSearch = googleSearch.toLowerCase();
+
+        searchImage();
     })
+}
 
-    //Google Image Fill
-
-    fetch("https://google-search3.p.rapidapi.com/api/v1/images/q=" + playerSearch, {
+//Google Image Fill
+function searchImage(){
+    fetch("https://google-search3.p.rapidapi.com/api/v1/images/q=" + googleSearch, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "google-search3.p.rapidapi.com",
 		"x-rapidapi-key": "f00811a9b8mshbde7c23c4a7c457p107154jsnad487331fa3c"
 	}
-})
-.then(function (response) {
-    return response.json();
-  })
-.then(function (data) {
-    // console.log(data.image_results[0].image.src);
-    for (var i = 0; i < data.length; i++) {
-        // console.log(data[i].name);
-        var userName = document
-        // userName.textContent=data[i].image_results.image;
-
-    }
-    picture1.setAttribute("src", data.image_results[0].image.src);
-    picture2.setAttribute("src", data.image_results[1].image.src);
-    picture3.setAttribute("src", data.image_results[2].image.src);
-    // picture4.setAttribute("src", data.image_results[3].image.src);
-})
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        // console.log(data.image_results[0].image.src);
+        for (var i = 0; i < data.length; i++) {
+            // console.log(data[i].name);
+            var userName = document
+            // userName.textContent=data[i].image_results.image;
+        }
+        console.log("https://google-search3.p.rapidapi.com/api/v1/images/q=" + googleSearch)
+        picture1.setAttribute("src", data.image_results[0].image.src);
+        picture2.setAttribute("src", data.image_results[1].image.src);
+        picture3.setAttribute("src", data.image_results[2].image.src);
+        // picture4.setAttribute("src", data.image_results[3].image.src);
+    })
 }
 
 searchBtn.click(searchPlayer);
